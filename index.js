490263c9d277bed6b9bc8158fd01bd703e03886b
Rey Mooy <gdrrey@gmail.com>
2022-02-02 00:45:40 +0800

    add more field

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(compression())
+const page = 'https://liquipedia.net/pubg/PUBG_Global_Championship/2021'
 
-const page = 'https://liquipedia.net/pubg/LOCO_War_of_Glory/Grand_Finals'
-
-app.get('/pmpl', async (req,res)=>{
+app.get('/tournament', async (req,res)=>{
   try {
 
     let tournament = {
@@ -28,13 +27,33 @@ app.get('/pmpl', async (req,res)=>{
 
     const $ = cheerio.load(response.body);
 
+    const getInfo = (parameter)=>{
+      let x = $('.infobox-cell-2').filter(function() {
+        return $(this).text().trim() === parameter;
+      }).next().text().trim();
+
+      if(x === '' || undefined) return undefined
+
+      return x
+    }
+
     tournament.title = $('h1.firstHeading').text()
-    tournament.prizePool = $('h1.firstHeading').text()
     tournament.logo = 'https://liquipedia.net' + $('.infobox-image > .center img').attr('src')
+    tournament.series = getInfo('Series:')
+    tournament.organizer = getInfo('Organizer:')
+    tournament.type = getInfo('Type:')
+    tournament.location = getInfo('Location:')
+    tournament.venue = getInfo('Venue:')
+    tournament.gameMode = getInfo('Game Mode:')
+    tournament.platform = getInfo('Platform:')
+    tournament.prizePool = getInfo('Prize Pool:')
+    tournament.startDate = getInfo('Start Date:')
+    tournament.endDate = getInfo('End Date:')
 
     $('div.teamcard').each((i,x)=>{
       tournament.teams[i] =  {name : $(x).find('center a').text()}
       tournament.teams[i].logo = 'https://liquipedia.net' + $(x).find('.logo img').attr('src')
+      tournament.teams[i].qualifier =  $(x).find('.teamcard-qualifier a').text()
       
       let players = []
       $(x).find('.list td > a').each((j,y)=> {
@@ -42,6 +61,8 @@ app.get('/pmpl', async (req,res)=>{
         tournament.teams[i].player = players
       })
     })
+
+    console.log(tournament);
 
     res.json(tournament)
   } catch (error) {
